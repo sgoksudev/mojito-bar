9530a1399fe52b5050253df350ea14513789c9cc
sgoksu <senagoksudev@gmail.com>
2025-07-25 10:40:05 +0300

    Implement coctails section

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+import gsap from "gsap";
+import { ScrollTrigger, SplitText } from "gsap/all";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Coctails from "./components/Coctails";
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
+const App = () => {
+  return (
+    <main>
+      <Navbar />
+      <Hero />
+      <Coctails />
+    </main>
+  );
+};
+
+export default App;
